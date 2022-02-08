b6d4f84e7df1b0d8b4ae5053b2d2e1a766b79298
riscaldo <riccardo85ferrara@gmail.com>
2022-02-08 13:05:21 +0700

    extract topics (terms for the moment) from text functions

--- controllers/storms.js
+++ controllers/storms.js
@@ -4,6 +4,22 @@ const express = require('express')
 const router = express.Router()
     // const Storms = require('../models/storms')
 
+// my functions 
+
+const getTopics = (text, n_topics = 1, n_terms = 5) => {
+    // this functions extracts terms to identify topics,
+    // for the moment we use the terms as topics,
+    //  we will adjust it in the future
+    var lda = require('lda');
+
+    // Extract sentences.
+    var documents = text.match(/[^\.!\?]+[\.!\?]+/g);
+
+    // Run LDA to get terms for n_topics (n_terms each).
+    var result = lda(documents, n_topics, n_terms);
+
+    console.log(result)
+}
 
 
 
@@ -33,6 +49,8 @@ router.post('/create', async(req, res, next) => {
             console.log('post request: create storm')
             let request_from_user_page = true
             if (request_from_user_page) {
+                let storm_text = 'Cats are small. Dogs are big. Cats like to chase mice. Dogs like to eat bones.'
+                let topics = getTopics(storm_text)
                 res.redirect('storms/list')
             } else {
                 res.redirect('storms/:username')
