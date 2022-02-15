d1dedce6533ff14bb57bca69fa97abd877635b17
riscaldo <riccardo85ferrara@gmail.com>
2022-02-15 12:36:49 +0700

    thunderize send thunder comment to db

--- controllers/thunders.js
+++ controllers/thunders.js
@@ -2,20 +2,39 @@
 const { query } = require('express');
 const express = require('express')
 const router = express.Router()
-    // const Thunders = require('../models/thunders')
+const Thunders = require('../models/thunders')
+const Storms = require('../models/storms')
 
 
 
 
 //Create requests GET / POST
-router.post('/', async(req, res, next) => {
+router.post('/create', async(req, res, next) => {
     try {
-        console.log('post request: thunders')
-        let request_from_user_page = true
-        if (request_from_user_page) {
-            res.redirect('storms/list')
+        if (!req.isAuthenticated()) {
+            res.redirect('auth/login')
         } else {
-            res.redirect('storms/:username')
+            console.log('\npost request: thunderize (Create a thunder)')
+                //to create a thunder the model neeeds:
+                // author, comment, trust_rate, freezed_skills, storm
+            req.body.author = req.user
+                // comment is already inside the req
+                // trust_rate is already inside the req
+            req.body.freezed_skills = req.user.skills
+            console.log('req: ', req.body)
+            let thunder = await Thunders.create(req.body)
+            if (thunder) {
+                console.log('thunder created')
+                console.log('id: ', thunder._id)
+                    // redirect:
+                let request_from_user_page = false
+                if (!request_from_user_page) {
+                    res.redirect('/')
+                } else {
+                    res.redirect('storms/:username')
+                }
+            }
+
         }
 
     } catch (err) {
