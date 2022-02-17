79498fc0248159265f1fa9489626f12f9fdfe726
riscaldo <riccardo85ferrara@gmail.com>
2022-02-17 10:13:06 +0700

    fix indentation

--- controllers/thunders.js
+++ controllers/thunders.js
@@ -15,18 +15,18 @@ router.post('/create', async(req, res, next) => {
             res.redirect('auth/login')
         } else {
             console.log('\npost request: thunderize (Create a thunder)')
-                //to create a thunder the model neeeds:
-                // author, comment, trust_rate, freezed_skills, storm
+            //to create a thunder the model neeeds:
+            // author, comment, trust_rate, freezed_skills, storm
             req.body.author = req.user
-                // comment is already inside the req
-                // trust_rate is already inside the req
+            // comment is already inside the req
+            // trust_rate is already inside the req
             req.body.freezed_skills = req.user.skills
             console.log('req: ', req.body)
             let thunder = await Thunders.create(req.body)
             if (thunder) {
                 console.log('thunder created')
                 console.log('id: ', thunder._id)
-                    // redirect:
+                // redirect:
                 let request_from_user_page = false
                 if (!request_from_user_page) {
                     res.redirect('/')
